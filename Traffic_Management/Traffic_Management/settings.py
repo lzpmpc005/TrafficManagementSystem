62398b464ee8837b021b7b63221bf46dc19abfbf
carimara <karimarabakh@gmail.com>
2024-02-06 03:21:01 +0500

    added email notification app

--- Traffic_Management/Traffic_Management/settings.py
+++ Traffic_Management/Traffic_Management/settings.py
@@ -14,7 +14,14 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'karinabakhti@gmail.com'
+EMAIL_HOST_PASSWORD = 'gyha qnrp rbht ebfu'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -78,9 +85,9 @@ WSGI_APPLICATION = "Traffic_Management.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Traffic_Management',
+        'NAME': 'db',
         'USER': 'root',
-        'PASSWORD': 'Fuchy0312',
+        'PASSWORD': 'Funchy0312',
         'HOST': 'localhost',  # or the hostname where your MySQL server is running
         'PORT': '3306',      # or the port on which your MySQL server is listening
     }
--- Traffic_Management/Traffic_Management/urls.py
+++ Traffic_Management/Traffic_Management/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('user_service/', include('User_Service.urls')),
+    path('notification/', include('notification.urls')),
 ]
